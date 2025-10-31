32a037a7ae59d7209041ea229d7878f19fceeff7
MD Mehedi Mostazir <plabonmostazir@gmail.com>
2025-10-31 21:54:24 +0600

    Flipped pyramid

---
+++ FlippedSimplePyramid.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cout<<"Enter the no of row : ";
+    cin>>n;
+
+    for (int i = n; i > 0; i--) {
+        for (int j = 1; j <= n; j++) 
+        {
+            if (j >= i) {
+                cout << "* ";
+            }
+            else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
